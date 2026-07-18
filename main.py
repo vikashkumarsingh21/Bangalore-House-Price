03299927659415a9672367ab9b5339e564fe1e42
vikashkumarsingh21'git config --global user.email vk0102103@gmail.com <vk0102103@gmail.com>
2026-07-18 19:02:48 +0530

    add fronted and backend

--- main.py
+++ main.py
@@ -1,10 +1,42 @@
-from flask import Flask, render_template, request
-
+import pandas as pd
+import pickle
+from flask import Flask, render_template,request
+import numpy as np
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+data = pd.read_csv("Cleaned_data.csv")
+data.columns = data.columns.str.strip()
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+pipe = pickle.load(open("RidgeModel.pkl", "rb"))
+
+@app.route('/')
+def home():
+
+    locations = sorted(data['location'].unique())
+
+    return render_template(
+        'index.html',
+        locations=locations,
+        prediction_text=""
+    )
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    location = request.form.get('location')
+    bhk = int(request.form.get('bhk'))
+    bath = float(request.form.get('bath'))
+    sqft = float(request.form.get('total_sqft'))
+
+    print(location, bhk, bath, sqft)
+
+    input_df = pd.DataFrame(
+    [[location, sqft, bath, bhk]],
+    columns=['location','total_sqft','bath','bhk']
+)
+    prediction = pipe.predict(input_df)[0] * 1e5
+
+    return str(round(prediction,2))
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
